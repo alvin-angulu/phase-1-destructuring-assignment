a9b96f790b2b6a13be685bcef247bc8b1dd04f07
Alvin Angulu <angulualvin@gmail.com>
2024-04-08 16:10:43 +0300

    lab completed

--- index.js
+++ index.js
@@ -26,14 +26,23 @@ const nestedMuppet = {
 };
 
 // Strings
-const [cowSound, horseSound, sheepSound, pigSound, chickenSound] = farmAnimals.split(' ');
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+
 
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-const [,blackAndWhite, black, pink] = farmAnimals.split(' ').filter(animal => animal !== 'chicken');
+
+
+
+// 3. Little the chicken had to go back to the coop, so now we're left with three.
+// Let's use color variables of blackAndWhite, black, and pink.
+//const [blackAndWhite, ,black, pink,] = farmAnimals.split(' ').filter(animal => animal !== 'chicken');
+
+const [blackAndWhite, ,black, pink,] = farmAnimals.split(' ');
+
 
 // Arrays
 
